509f7fff1783bb1aa5601482f1e8858339bca59f
fsntico <fsntico@gmail.com>
2017-05-29 23:44:04 -0300

    Add files via upload

---
+++ aula02.py
@@ -0,0 +1,14 @@
+num_int = 5
+num_dec = 7.3
+val_str = "qualquer texto"
+print("o valor é:", num_int)
+print("o valor é: %i" % num_int)
+print("o valor é: " + str(num_int))
+
+print("Concatendo decimal:", num_dec)
+print("Cancatendo decimal: %.10f" % num_dec)
+print("Cancatendo decimal:" + str(num_dec))
+
+print("Concatendo string", val_str)
+print("Concatendo string %s" %val_str)
+print("Concatendo string " + val_str)
---
+++ teste01.py
@@ -0,0 +1,4 @@
+num = 0
+if (num == 0):
+    print("Olá mundo")
+    print("Teste em python")
