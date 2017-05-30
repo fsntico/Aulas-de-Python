84235bae83a298250c7e0d209d10e33de3303d4b
Francisco Sita <fsntico@gmail.com>
2017-05-29 23:42:55 -0300

    mais aulas
    mais aulas

---
+++ login_.py
@@ -0,0 +1,6 @@
+login = input("Login:") #variável login, recebe input(ou seja, precisa adicionar um valor, a string "login" é na tela.
+senha = input("Senha:")
+
+
+print("O usuário informado foi: %s, e a senha digitada foi: %s" %(login, senha))
+#%s serve para adicionar uma string de uma variável. %(login é o primeiro marcador, e "senha" é o segundo marcador).
